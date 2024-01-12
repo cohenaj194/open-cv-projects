8107ef8ee41bb79d2a6fc0e5b6d6dbf72f4d7988
MSTAM-MDC <99449410+MSTAM-MDC@users.noreply.github.com>
2024-01-12 14:47:51 -0500

    update to app 1
    Replaced image path, enhanced code readability on variables for eyes, created resizable window when running app 1

--- 1/app.py
+++ 1/app.py
@@ -6,7 +6,11 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
 # Load an image
-image = cv2.imread('Human_faces.jpg')  # Replace with your image path
+image = cv2.imread(r'C:\Users\MSTAM\OneDrive\Documents\GitHub\open-cv-projects\1\Human_faces.jpg')  # Replace with your image path
+
+if image is None:
+    raise ValueError("Image not found or cannot be loaded.")
+
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the image
@@ -21,11 +25,12 @@ for (x, y, w, h) in faces:
     
     # Detect eyes within the face region
     eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex, ey, ew, eh) in eyes:
+    for (eye_x, eye_y, eye_w, eye_h) in eyes:
         # Draw a rectangle around the eyes
-        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        cv2.rectangle(roi_color, (eye_x, eye_y), (eye_x+eye_w, eye_y+eye_h), (0, 255, 0), 2)
 
 # Display the result
+cv2.namedWindow('Facial Feature Art', cv2.WINDOW_NORMAL)
 cv2.imshow('Facial Feature Art', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
