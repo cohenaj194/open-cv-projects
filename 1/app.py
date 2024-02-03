8536b15c270548cf55b3fe2d32b4a9ca659477cc
MSTAM-MDC <99449410+MSTAM-MDC@users.noreply.github.com>
2024-02-03 09:27:04 -0500

    Update

--- 1/app.py
+++ 1/app.py
@@ -1,16 +1,6 @@
 import cv2
 import numpy as np
 
-# Flag to indicate if the application should be closed
-#exit_flag = False
-
-# Function to handle keyboard events
-#def keyboard_event_handler(event):
-#    global exit_flag
-
-#    if event == ord('e'):
-#        exit_flag = True
-
 # Load the pre-trained Haar Cascades for face and eye detection
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
@@ -43,24 +33,5 @@ for (x, y, w, h) in faces:
 cv2.namedWindow('Facial Feature Art', cv2.WINDOW_NORMAL)
 cv2.imshow('Facial Feature Art', image)
 # Set the keyboard event callback function
-#cv2.setMouseCallback('Facial Feature Art', keyboard_event_handler)
-
-#while not exit_flag:
-    # Display the image
-#    cv2.imshow('Facial Feature Art', image)
-
-#    key = cv2.waitKey(1)
-#    if key == ord('f') and cv2.getWindowProperty('Facial Feature Art', cv2.WND_PROP_VISIBLE):
-        # Create a file menu
-#        menu = np.zeros((50, 200, 3), np.uint8)
-#        menu[:] = (255, 255, 255)
-
-        # Display "Close" option in the file menu
-#        cv2.putText(menu, 'Close (Alt + e)', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
-
-#        cv2.imshow('File Menu', menu)
-#    elif key == 27 or key == ord('e'):
-        # Close the file menu
-#        cv2.destroyWindow('File Menu')
 cv2.waitKey(0)
 cv2.destroyAllWindows()
